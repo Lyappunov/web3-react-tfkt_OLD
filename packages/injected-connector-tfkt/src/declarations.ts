45cb5d695e5c375895e4379906634bcce217e454
Lyappunov <lyappunov19@outlook.com>
2021-08-01 20:36:55 -0700

    update repository and add new package (injected-connector-tfkt)

---
+++ packages/injected-connector-tfkt/src/declarations.ts
@@ -0,0 +1,12 @@
+interface Dexon {
+  send: unknown
+  enable: () => Promise<string[]>
+  on?: (method: string, listener: (...args: any[]) => void) => void
+  removeListener?: (method: string, listener: (...args: any[]) => void) => void
+}
+
+declare interface Window {
+  dexon?: Dexon
+}
+
+declare const __DEV__: boolean
